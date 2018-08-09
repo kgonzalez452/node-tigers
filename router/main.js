e23040a2d29801fc7aa67813ab628338b707b8ca
kgonzalez452 <kgonzalez@techlaunch.io>
2018-08-08 22:38:43 -0400

    added express-session and cookie parser functions for login and logout

--- router/main.js
+++ router/main.js
@@ -1,36 +1,62 @@
-module.exports = function(app, tigers) {
-  app.get("/", function(req, res) {
-    res.render("index", { ptitle: "Tigers", tigers: tigers });
+module.exports = function (app, tigers) {
+  app.get('/', function (req, res) {
+    if (req.session.username) {
+      res.render('index', { ptitle: "Tigers", tigers: tigers, username: req.session.username });
+    }
+    else res.redirect('/login');
   });
 
-  app.get("/add", function(req, res) {
-    res.render("add", { ptitle: "Add tiger" });
+  app.get('/add', function (req, res) {
+    if (req.session.username) {
+      res.render('add', { ptitle: "Add tiger", username: req.session.username });
+    }
+    else res.redirect('/login');
   });
 
-  app.get("/add-submit", function(req, res) {
+  app.get('/add-submit', function (req, res) {
     tigers.push(req.query.name);
-    res.redirect("/");
+    res.redirect('/');
   });
 
-  app.get("/update", function(req, res) {
-    res.render("update", { ptitle: "Update tiger" });
+  app.get('/update', function (req, res) {
+    if (req.session.username) {
+      res.render('update', { ptitle: "Update tiger", username: req.session.username });
+    }
+    else res.redirect('/login');
   });
 
-  app.get("/update-submit", function(req, res) {
+  app.get('/update-submit', function (req, res) {
     tigers.splice(req.query.pos, 1, req.query.name);
-    res.redirect("/");
+    res.redirect('/');
   });
 
-  app.get("/del", function(req, res) {
-    res.render("delete", { ptitle: "Delete tiger" });
+  app.get('/del', function (req, res) {
+    if (req.session.username) {
+      res.render('delete', { ptitle: "Delete tiger", username: req.session.username });
+    }
+    else res.redirect('/login');
   });
 
-  app.get("/del-submit", function(req, res) {
+  app.get('/del-submit', function (req, res) {
     tigers.splice(req.query.pos, 1);
-    res.redirect("/");
+    res.redirect('/');
+  });
+
+  app.get('/login', function (req, res) {
+    res.render('login', { ptitle: "Login", hideLogin: true });
+  });
+
+  app.get('/login-submit', function (req, res) {
+    req.session.username = req.query.name;
+    res.redirect('/');
+  });
+
+  app.get('/logout-submit', function (req, res) {
+    req.session.username = false;
+    res.send('You ended your session correctly');
   });
 
   app.get('/get-tigers', function (req, res) {
     res.json({ tigers: tigers });
   });
-};
+}
